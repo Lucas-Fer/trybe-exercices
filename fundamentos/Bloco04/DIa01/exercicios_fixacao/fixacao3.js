d00a9adb046eb74f0ec9d64307b4dce234b29daa
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-25 17:45:30 -0400

    Resolvendo o terceiro exercicio de fixaçao

---
+++ fundamentos/Bloco04/DIa01/exercicios_fixacao/fixacao3.js
@@ -0,0 +1,13 @@
+const nota = 55;
+
+if(nota >= 80){
+  console.log("Parabén, você foi aprovado(a)!");
+}
+
+else if(nota <= 80 && nota >= 60){
+  console.log("Você está na nossa lista de espera!");
+}
+
+else{
+  console.log("Infelizmente você foi reprovado, tente novamente!");
+}
