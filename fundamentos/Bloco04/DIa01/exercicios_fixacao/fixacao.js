9696f70b746cda7106b2decd912e33e6d61c6338
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-25 17:23:49 -0400

    realizando o primeiro exercicio de fixaçao

---
+++ fundamentos/Bloco04/DIa01/exercicios_fixacao/fixacao.js
@@ -0,0 +1,22 @@
+/* exercício 1, 2, 3 e 4
+const myName = 'Lucas'
+const birthCity = 'Acre';
+let birthYear = 2000;
+
+console.log(myName, birthYear, birthCity); */
+
+/* EXERCICIO 5
+const myName = 'Lucas'
+const birthCity = 'Acre';
+let birthYear = 2000;
+birthYear = 2030;
+
+console.log(birthYear); */
+
+const myName = 'Lucas'
+const birthCity = 'Acre';
+birthCity = "Rondônia";
+// Aqui a nova variável de birthCity não será aplicada, visto que ela é constante, como foi definido.
+let birthYear = 2000;
+
+console.log(birthCity);
