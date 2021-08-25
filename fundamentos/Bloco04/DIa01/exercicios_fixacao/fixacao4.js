c6ea65768f482b25b5b140e3df0e06bfe22f0afc
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-25 18:41:43 -0400

    Completa o quarto exercicio parte I e II

---
+++ fundamentos/Bloco04/DIa01/exercicios_fixacao/fixacao4.js
@@ -0,0 +1,44 @@
+/* PARTE 1 - Operador AND
+const currentHour = 1;
+let message = '';
+
+if(currentHour >= 22){
+  message = "Não deveríamos comer nada, é hora de dormir"
+  console.log(message);
+}
+
+else if(currentHour >= 18 && currentHour < 22){
+  message = "Rango da noite, vamos jantar :D";
+  console.log(message); 
+}
+
+else if(currentHour >= 14 && currentHour < 18){
+  message = "Vamos fazer um bolo pro café da tarde?";
+  console.log(message);
+}
+
+else if(currentHour >= 11 && currentHour < 14){
+  message = "Hora do almoço!!!";
+  console.log(message);
+}
+
+else if(currentHour >= 2 && currentHour < 11) {
+  message = "Hmmm, cheiro de café recém passado";
+  console.log(message);
+}
+else{
+  console.log("Condições não atendidas");
+}
+*/
+
+
+/* PARTE 2- Operador OR 
+let weekDay = "segunda-feira";
+
+if(weekDay === "segunda-feira" || weekDay ==="terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else{
+  console.log("Liberdade!");
+}
+*/
