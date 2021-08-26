9cac9038f372e1bc4f84f8bbe6c07474dc5749ab
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-25 21:28:49 -0400

    Realizando os 5 programas do primeiro exercicio

---
+++ fundamentos/Bloco04/DIa01/exercicio-course/programas.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 5;
+
+let adicao = a + b;
+console.log("A soma dos valore é de: " + adicao);
+
+let subtracao = a - b;
+console.log("A subtração dos valores é de: " + subtracao);
+
+let multiplicacao = a * b;
+console.log("A multiplicação dos valores é de: " + multiplicacao);
+
+let divisao = a / b;
+console.log("A divisão dos valores é de: " + divisao);
+
+let modulo = a % b;
+console.log("O módulo dos valores é de: " + modulo);
