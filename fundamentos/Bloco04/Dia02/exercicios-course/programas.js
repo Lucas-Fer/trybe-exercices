4c6fe9a99a073b3077cf25029994b3db7007fa3d
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 18:02:59 -0400

    Resolve o exercicio 5

--- fundamentos/Bloco04/Dia02/exercicios-course/programas.js
+++ fundamentos/Bloco04/Dia02/exercicios-course/programas.js
@@ -1,23 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-soma = 0;
 
-for(contador = 0; contador < numbers.length; contador += 1){
-  //aqui será armazenado a soma
-  soma = soma + numbers[contador];
 
+//defino maior valor como 0
+maiorValor = 0;
+for(let indice = 0; indice < numbers.length; indice += 1){
+  //se o valor do indice atual for maior que maiorValor (que é 0)
+  if(numbers[indice] > maiorValor)
+
+  maiorValor = numbers[indice];
+    //o maior valor recebe o número do índice atual, caso seja maior.
+  //o maior valor tinha o valor de 0 e o índice inicial tem o valor de 5. Logo o maior valor vai sair de 0 e receber o 5. E assim vai
 }
 
-  mediaAritmetica = soma/contador;
-  console.log("A soma é de: " + soma);
-  console.log("A média aritmética é de: " + mediaAritmetica);
-
-  if(mediaAritmetica > 20){
-    console.log("Valor maior que 20");
-  }
-
-  else{
-    console.log("Valor menor ou igual a 20");
-  }
+console.log("O maior valor é de: " + maiorValor);
 
 
