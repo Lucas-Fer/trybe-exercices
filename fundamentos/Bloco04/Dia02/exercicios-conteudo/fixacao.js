732eec36a287debf9ecb2ca4c933c58d1d54893d
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 14:40:30 -0400

    Completando: FOR/OF

--- fundamentos/Bloco04/Dia02/exercicios-conteudo/fixacao.js
+++ fundamentos/Bloco04/Dia02/exercicios-conteudo/fixacao.js
@@ -8,9 +8,17 @@ console.log(menuServices);
 console.log("O índice de Portfólio é: " + indexOfPortfolio);
 console.log(menu);
 */
-
+/*
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
 for(let indice = 0; indice < groceryList.length; indice += 1){
   console.log(groceryList[indice]);
+}
+
+*/
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let itemNames of names){
+    console.log(itemNames);
 }
