5b477316192cbb15fb1c4fa10cfca89a6b63794a
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 14:32:06 -0400

    Completando Array(Listas)

---
+++ fundamentos/Bloco04/Dia02/exercicios-conteudo/fixacao.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio') ;
+let menuServices = menu[1];
+menu.push('Contato');
+
+
+console.log(menuServices);
+console.log("O índice de Portfólio é: " + indexOfPortfolio);
+console.log(menu);
