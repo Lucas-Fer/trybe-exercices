b5bb6c3cb83ee36538d40533933d8e6c3782e70b
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 14:37:52 -0400

    Completando: FOR

--- fundamentos/Bloco04/Dia02/exercicios-conteudo/fixacao.js
+++ fundamentos/Bloco04/Dia02/exercicios-conteudo/fixacao.js
@@ -1,4 +1,4 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio') ;
 let menuServices = menu[1];
 menu.push('Contato');
@@ -7,3 +7,10 @@ menu.push('Contato');
 console.log(menuServices);
 console.log("O índice de Portfólio é: " + indexOfPortfolio);
 console.log(menu);
+*/
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let indice = 0; indice < groceryList.length; indice += 1){
+  console.log(groceryList[indice]);
+}
