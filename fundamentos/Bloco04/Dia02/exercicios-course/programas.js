fa8d952f2970c4c44e23d17abbe250eb118cbe13
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 18:09:50 -0400

    Arrumando os exercicios no arquivo

--- fundamentos/Bloco04/Dia02/exercicios-course/programas.js
+++ fundamentos/Bloco04/Dia02/exercicios-course/programas.js
@@ -1,7 +1,57 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+/*exercicio 1
+console.log(numbers);
+---------------------------------------
+*/
 
+/*
+EXERCICIO 2
+soma = 0;
 
+for(contador = 0; contador < numbers.length; contador += 1){
+  //aqui será armazenado a soma
+  soma = soma + numbers[contador];
+
+}
+console.log(soma);
+------------------------------
+*/
+/* 
+EXERCICIO 3
+for(contador = 0; contador < numbers.length; contador += 1){
+  //aqui será armazenado a soma
+  soma = soma + numbers[contador];
+}
+
+mediaAritmetica = soma/contador;
+console.log(soma);
+console.log(mediaAritmetica);
+
+---------------------------------
+*/
+/*
+EXERCICIO 4
+for(contador = 0; contador < numbers.length; contador += 1){
+  //aqui será armazenado a soma
+  soma = soma + numbers[contador];
+
+  mediaAritmetica = soma/contador;
+  console.log("A soma é de: " + soma);
+  console.log("A média aritmética é de: " + mediaAritmetica);
+
+  if(mediaAritmetica > 20){
+    console.log("Valor maior que 20");
+  }
+
+  else{
+    console.log("Valor menor ou igual a 20");
+  }
+-----------------------
+*/
+
+/*
+EXERCICIO 5
 //defino maior valor como 0
 maiorValor = 0;
 for(let indice = 0; indice < numbers.length; indice += 1){
@@ -15,4 +65,4 @@ for(let indice = 0; indice < numbers.length; indice += 1){
 
 console.log("O maior valor é de: " + maiorValor);
 
-
+*/
