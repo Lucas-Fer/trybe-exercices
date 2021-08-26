4d90f2d42e5ca6a5f86f7040bf082b7005df63d2
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-08-26 17:30:49 -0400

    Completando o exercicio 5

--- fundamentos/Bloco04/Dia02/exercicios-course/programas.js
+++ fundamentos/Bloco04/Dia02/exercicios-course/programas.js
@@ -5,10 +5,19 @@ soma = 0;
 for(contador = 0; contador < numbers.length; contador += 1){
   //aqui será armazenado a soma
   soma = soma + numbers[contador];
-  
 
 }
 
-mediaAritmetica = soma/contador;
-console.log(soma);
-console.log(mediaAritmetica);
+  mediaAritmetica = soma/contador;
+  console.log("A soma é de: " + soma);
+  console.log("A média aritmética é de: " + mediaAritmetica);
+
+  if(mediaAritmetica > 20){
+    console.log("Valor maior que 20");
+  }
+
+  else{
+    console.log("Valor menor ou igual a 20");
+  }
+
+
