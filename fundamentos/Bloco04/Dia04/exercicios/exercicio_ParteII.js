7e6a200bdc45a42911e36060e45ab52fbe923165
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-09-01 10:46:19 -0400

    resolve o exercicio 3

--- fundamentos/Bloco04/Dia04/exercicios/exercicio_ParteII.js
+++ fundamentos/Bloco04/Dia04/exercicios/exercicio_ParteII.js
@@ -1,4 +1,4 @@
-//criar a função para receber string e retornar true se for palindromo
+/*//criar a função para receber string e retornar true se for palindromo
 // laço de repetição para pecorrer a metade do array
 // forma de pecorrer cada letra
 // estrutura de condição para comparar as metades
@@ -11,4 +11,31 @@ function verificaPalindrome(string) {
     return false;
   }
 }
-console.log(verificaPalindrome("ararasa"));
+console.log(verificaPalindrome("ararasa"));*/
+/*
+function indiceDoMaior(numeros) {
+  let indiceMaior = 0;
+  for (let indice in numeros) {
+    if (numeros[indiceMaior] < numeros[indice]) {
+      indiceMaior = indice;
+    }
+  }
+  return indiceMaior;
+}
+
+console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4*/
+
+function menorIndice(numbers){
+  //pecorrer em um laço de repetição o array de numeros
+  //definir uma variavel menorINdice que sera 0
+
+  indiceMenor = 0;
+  for(let indice in numbers){
+    //como o indiceMenor é 0, então está no primeiro indice
+    if(numbers[indiceMenor] > numbers[indice]){
+      indiceMenor = indice;
+    }
+  }
+  return indiceMenor;
+}
+console.log(menorIndice([1,2,-3,6,7]));
