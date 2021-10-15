29a719989d61243297a3928406cd5f85f6362687
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-15 15:07:03 -0400

    completa o exercico do conteudo assincrono: lidando com erros em exerçoes

--- fundamentos/Bloco09/conteudo-assincrono/script.js
+++ fundamentos/Bloco09/conteudo-assincrono/script.js
@@ -35,3 +35,39 @@ const getUser = (callback) => {
 
 getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
 getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+
+
+//OPERAÇÕES ASSINCRONAS
+
+// 1 - A função getCountry abaixo tem aproximadamente 50% de chance de obter com sucesso um país, tendo um callback para poder ser feita qualquer operação sobre o país retornado. Adicione um outro callback para getCountry , de forma que trate a mensagem de erro retornada.
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFail) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if (didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      return onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      return onFail(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
