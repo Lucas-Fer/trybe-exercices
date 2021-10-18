1dd9da342074fc057a2110970194a697ef6a3e94
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-18 10:42:09 -0400

    set: requisito 4

--- fundamentos/Bloco09/exercicio-vigente/script.js
+++ fundamentos/Bloco09/exercicio-vigente/script.js
@@ -59,3 +59,21 @@ const getPlanet = () => {
 
 getPlanet(); // imprime Marte depois de 4 segundos
 
+// 4 - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature , que imprime a temperatura em Marte.
+
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// // crie a função sendMarsTemperature abaixo
+// const sendMarsTemperature = () => {
+//   setTimeout(() => {
+//     robotMessage = '[Marte]Robô Curiosity para Terra: ';
+//     console.log(`${robotMessage} A temperatura atual de Marte é de -> ${getMarsTemperature()} graus`);
+//   }, messageDelay());
+// };
+// sendMarsTemperature();
+
