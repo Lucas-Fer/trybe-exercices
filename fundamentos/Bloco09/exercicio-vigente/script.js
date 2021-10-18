d3e87b97a91fef3a1c6a508cda26cf2d3b15e599
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-18 10:41:53 -0400

    set: requisito 3

--- fundamentos/Bloco09/exercicio-vigente/script.js
+++ fundamentos/Bloco09/exercicio-vigente/script.js
@@ -42,3 +42,20 @@ console.log(planetDistanceFromSun(mars));
 setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000);
 setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000);
 
+// 3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.;
+
+const getPlanet = () => {
+  setTimeout(() => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    console.log("Returned planet: ", mars);
+  }, 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
+
