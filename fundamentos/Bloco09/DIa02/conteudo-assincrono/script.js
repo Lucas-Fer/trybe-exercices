6f7055973283f82eaaa28d19a413ff92b1416cab
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-18 17:54:17 -0400

    set: questao 1

---
+++ fundamentos/Bloco09/DIa02/conteudo-assincrono/script.js
@@ -0,0 +1,15 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+const h2 = document.querySelector('#jokeContainer');
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data =>
+      document.getElementById('jokeContainer').innerText = data.joke
+    ).catch((error) => console.log(error));
+};
+
+window.onload = () => fetchJoke();
