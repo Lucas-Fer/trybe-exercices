9db1a0aa6d4575c20ffe78f17975779223b2df73
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-18 10:41:08 -0400

    set: requisito 1

---
+++ fundamentos/Bloco09/exercicio-vigente/script.js
@@ -0,0 +1,35 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+
+// 1 - Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+// R: primeiro 'Mars', depois Venus' e por último 'Jupiter'.
+
+console.log(planetDistanceFromSun(mars));
+console.log(planetDistanceFromSun(venus));
+console.log(planetDistanceFromSun(jupiter));
+
