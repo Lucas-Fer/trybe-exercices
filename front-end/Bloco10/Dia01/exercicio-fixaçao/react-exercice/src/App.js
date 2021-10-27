a61a9a49206158e811d0079ec63f8e0f58dbee87
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-27 18:19:30 -0400

    implement(questao 2): lista de tarefas dinamica

---
+++ front-end/Bloco10/Dia01/exercicio-fixaçao/react-exercice/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import TaskList from './taskList';
+
+class App extends Component {
+  render() {
+    return (
+      <TaskList />
+    );
+  }
+}
+
+export default App;
---
+++ front-end/Bloco10/Dia01/exercicio-fixaçao/react-exercice/src/taskList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class TaskList extends Component {
+  render() {
+    const arrayTask = ['Estudar', 'Lazer', 'Compromisso', 'Cuidar dos cachorros'];
+
+    return (
+      <section>
+        <h1>Minha lista de Tarefas</h1>
+        <ol>
+          {arrayTask.map((arrayIndex) => <li>{arrayIndex}</li>)}
+        </ol>
+      </section>
+    );
+  }
+}
+export default TaskList;
