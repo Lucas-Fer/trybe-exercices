0cea98fe422966d56f6c9517e6d99cd77261468d
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-27 18:29:18 -0400

    implement(questao 3): Header

--- front-end/Bloco10/Dia01/exercicio-fixaçao/react-exercice/src/App.js
+++ front-end/Bloco10/Dia01/exercicio-fixaçao/react-exercice/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { Component } from 'react';
 import TaskList from './taskList';
-
+import Header from './Header';
 class App extends Component {
   render() {
     return (
-      <TaskList />
+      <>
+        <Header />
+        <TaskList />
+      </>
     );
   }
 }
