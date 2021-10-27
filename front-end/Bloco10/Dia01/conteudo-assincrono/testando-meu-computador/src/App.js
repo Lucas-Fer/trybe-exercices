2d2055e579dc7a9cf59b44ecfbfb5162d94cde63
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-27 14:52:37 -0400

    faz o Hello World no conteudo assincrono

---
+++ front-end/Bloco10/Dia01/conteudo-assincrono/testando-meu-computador/src/App.js
@@ -0,0 +1,8 @@
+import AboutMe from './Component';
+import './App.css';
+
+function App() {
+  return (<AboutMe></AboutMe>);
+}
+
+export default App;
---
+++ front-end/Bloco10/Dia01/conteudo-assincrono/testando-meu-computador/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lucas Sousa Fernandes</h1>
+        <p>Meu primeiro componente em React</p>
+      </div>
+    );
+  }
+}
+export default AboutMe;
