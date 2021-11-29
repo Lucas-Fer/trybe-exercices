5df399e7b8d4a23804b3b4bf7c2fac98691c3ee8
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-11-29 13:48:22 -0400

    .

--- front-end/Bloco14/DIa02/exercicio-fixacao/src/service.js
+++ front-end/Bloco14/DIa02/exercicio-fixacao/src/service.js
@@ -1,1 +1,13 @@
-const retornaNumeroAleatorio = () => Math.floor(Math.random() * 100);
+const randomNumber = () => Math.floor(Math.random() * 100);
+// console.log(randomNumber());
+const upperString = (string) => string.toUpperCase();
+const getFirstLetra = (string) => string[0];
+const twoStringsTogether = (stringA, stringB) => stringA + stringB;
+
+function fetchApi() {
+  return fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json()
+      .then(json => response.ok ? Promise.resolve(json) : Promise.reject(json))
+    );
+}
+module.exports = { randomNumber, upperString, getFirstLetra, twoStringsTogether, fetchApi };
